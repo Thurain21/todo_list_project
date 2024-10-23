e962512f939c20f4823802f3fc247635e736a987
Thurain21 <thurainkosg@gmail.com>
2024-10-23 19:27:00 +0800

    add jason server and axios

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import List from './components/List';
+import Form from './components/Form';
 
 const App = () => {
   return (
     <div className='mt-5'>
+      <Form/>
         <List/>
     </div>
   )
