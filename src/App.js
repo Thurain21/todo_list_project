cff4c7df5e952fc1df28c00403ee3e3cf4611db1
Thurain21 <thurainkosg@gmail.com>
2024-10-25 18:45:30 +0800

    add api call

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import List from './components/List';
 import Form from './components/Form';
+import { apidata } from './api/apiResource';
 
 const App = () => {
+
+    const fetchdata = async() => {
+        const data = await apidata.create('/todolist');
+
+        console.log(data);
+        
+    } 
+
   return (
     <div className='mt-5'>
       <Form/>
