3980282d6adab0e38f6c8df6fc0fb376ed99addb
Thurain21 <thurainkosg@gmail.com>
2024-10-29 20:55:06 +0800

    add list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,9 +4,9 @@ import Cart from "./Cart";
 const List = ({tasks}) => {
   return (
     <div>
-      <div className="w-50 mx-auto">
-        <ol className="list-group mt-3">
-          <Cart data={tasks}/>
+      <div className="w-50 mx-auto mt-3">
+        <ol className="list-group ">
+          <Cart tasks={tasks}/>
         </ol>
       </div>
     </div>
