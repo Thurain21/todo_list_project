83edce428a33e390830418b576cae41ca122bb1c
Thurain21 <thurainkosg@gmail.com>
2024-10-30 18:08:24 +0800

    add update function

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Cart from "./Cart";
 
-const List = ({tasks , deleteTask}) => {
+const List = ({tasks , deleteTask ,updateTask}) => {
   return (
     <div>
       <div className="w-50 mx-auto mt-3">
         <ol className="list-group ">
-          <Cart tasks={tasks} deleteTask={deleteTask}/>
+          <Cart tasks={tasks} deleteTask={deleteTask} updateTask = {updateTask}/>
         </ol>
       </div>
     </div>
